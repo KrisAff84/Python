4482df8447431c0db584305651b95ddb7c85bf05
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2023-06-01 00:31:25 -0500

    Added new files

---
+++ Refund-Policy-Helper.py
@@ -0,0 +1,11 @@
+days_purchased = int(input('How many days ago have you purchased the item? '))
+used = input('Have you used the item at all [y/n]? ')
+broken = input('Has the item broken down on its own [y/n]? ')
+if (days_purchased < 11 and used == 'n') or (broken == 'y'):
+    print('You can get a refund.')
+else:
+    print('You cannot get a refund.')
+
+
+
+
