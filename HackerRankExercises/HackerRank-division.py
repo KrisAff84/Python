36b3d5edb1bbd21fa360782038e378c25cd96947
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2023-06-02 12:48:45 -0500

    Added new files

---
+++ HackerRankExercises/HackerRank-division.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input('Enter a number: '))
+    b = int(input('Enter a second number: '))
+print(a // b)
+print(a / b)
+
+
+
---
+++ HackerRankExercises/HackerRank-leap-year-function.py
@@ -0,0 +1,11 @@
+def is_leap(year):
+    leap = False
+year = int(input())
+if (year % 4 == 0) and (year % 100 != 0) and (year % 400 == 0):
+    print('True')
+
+
+
+
+
+year = int(input())
