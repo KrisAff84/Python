6f66e7d576184c2cf08d1d26302c366b4ecc999b
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2023-06-13 12:41:18 -0500

    Modified project files

--- Python_Projects/file_dict_function.py
+++ Python_Projects/file_dict_function.py
@@ -58,9 +58,8 @@ def modified(path1, path2):
     return modified
 
 
-# Prompt for path to catalog
-# Prints current directory path
-def file_dict(path=os.getcwd()):
+
+def filedict(path=os.getcwd()):
     while True:
         print()
         if os.path.exists(path):
@@ -69,7 +68,8 @@ def file_dict(path=os.getcwd()):
             print()
             break
         else:
-            print('Please enter a valid path!')
+            print('Path not valid!')
+            break
 
     # Adds files and directories recursively to dictionary file_dict
     file_dict = {}
@@ -94,50 +94,10 @@ def file_dict(path=os.getcwd()):
                     f'Size: {dir_size}', \
                     'Created: ' + dir_created, \
                     'Last Modified: ' + dir_mod_time
-    total_items = len(file_dict)
 
-    # Prints files and directories in current directory (non-recursive)
-    print('************ ' + Format.underline + 'Files and directories in current directory' + Format.end + ' ************')
+    print(f'Total Items: {len(file_dict)}')
     print()
-    print(f'Total items in directory: {total_items}')
-    print()
-    print(Format.underline + 'DIRECTORIES:' + Format.end)
-    for key in file_dict.keys():
-        if isdir(join(path, key)):
-            print(key)
-    print()
-    print(Format.underline + 'FILES:' + Format.end)
-    for key in file_dict.keys():
-        if isfile(join(path, key)):
-            print(key)
-    print()
+    for key, value in file_dict.items():
+        print(Format.green + key + Format.end, ' : ', value)
 
-    # User prompt for more information about a given file or directory
-    # Will list key and values of file_dict based on user input and display various information
-    # Lists directories recursively
-    amended_path = path
-    while True:
-        user_input = input('Enter a file or directory name to get information about it, or type ALL or EXIT: ')
-        print()
-        if user_input == 'EXIT':
-            print('Goodbye for now!')
-            break
-        elif user_input in file_dict:
-            if isdir(join(path, user_input)):
-                print(Format.green + user_input + Format.end, ' : ', file_dict[user_input])
-                print('Files and sub-directories: ', os.listdir(join(path, user_input)))
-                amended_path = join(path, user_input)
-                print()
-            elif isdir(join(amended_path, user_input)):
-                print(Format.green + user_input + Format.end, ' : ', file_dict[user_input])
-                print('Files and sub-directories: ', os.listdir(join(amended_path, user_input)))
-                amended_path = join(amended_path, user_input)
-                print()
-            else:
-                print(Format.green + user_input + Format.end, ' : ', file_dict[user_input])
-        elif user_input == 'ALL':
-            for key, value in file_dict.items():
-                print(Format.green + key + Format.end, ' : ', value)
-        else:
-            print('File not found!')
-        print()
+    return file_dict
--- Python_Projects/invoke-user-functions.py
+++ Python_Projects/invoke-user-functions.py
@@ -1,4 +1,4 @@
 from random_name_function import *
 from file_dict_function import *
 
-file_dict()
+filedict()
