f19eb1730ed2987c1384b222632dc4c981b57b00
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2023-06-14 00:34:21 -0500

    Re-organized + added new files

---
+++ Notes/lists.py
@@ -0,0 +1,4 @@
+# Another way of creating an empty list
+
+fruits = [] # The way we know already, or
+fruit = list()
---
+++ Notes/strings.py
@@ -0,0 +1,7 @@
+# Format
+
+first_name = 'Kris'
+last_name = 'Afflerbaugh'
+
+print('My first name is {}. My last name is {}'. format(first_name, last_name))
+
